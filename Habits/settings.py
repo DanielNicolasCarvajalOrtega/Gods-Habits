52dde92ed9cf1ff8ebb29aeacde48f8db5fbf89a
Nicolas.py <nikoceva2@gmail.com>
2025-08-14 23:58:25 -0400

    ORM modeling and DB modeling are added.

--- Habits/settings.py
+++ Habits/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "Users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "Users.UserProfile"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
---
+++ Users/models.py
@@ -0,0 +1,116 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
+
+
+class ModelUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+        # TIPO DE USUARIO
+    USER_TYPE_CHOICHES = [
+        ('beginner', 'Principiante'),
+        ('intermediate', 'Intermedio'),
+        ('advanced', 'Avanzado'),
+        ('athlete', 'Deportista Alto Rendimiento'),
+        ('professional', 'Profesional Ocupado'),
+        ('student', 'Estudiante'),
+    ]
+        # AREAS DE FOCO
+    FOCUS_AREA_CHOICES = [
+        ('fitness', 'Entrenamiento Físico'),
+        ('health', 'Salud y Bienestar'),
+        ('learning', 'Lectura y Aprendizaje'),
+        ('work', 'Productividad Laboral'),
+        ('creativity', 'Creatividad y Arte'),
+        ('mindfulness', 'Mindfulness y Meditación'),
+        ('social', 'Relaciones Sociales'),
+        ('finance', 'Finanzas Personales'),
+    ]
+
+        # TIEMPO A DEDICAR
+    TIME_AVAILABILITY_CHOICES = [
+        ('low', 'Poco tiempo (< 1 hora/día)'),
+        ('medium', 'Tiempo moderado (1-3 horas/día)'),
+        ('high', 'Mucho tiempo (> 3 horas/día)'),
+        ('flexible', 'Horario flexible'),
+    ]
+
+    user_type = models.CharField(max_length=70, choices=USER_TYPE_CHOICHES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_focus_area = models.CharField(max_length=100, choices=FOCUS_AREA_CHOICES)
+    secondary_focus_area = models.JSONField(default=list, blank=True, help_text="Otras areas de foco")
+    daily_time_availability = models.CharField(max_length=100, choices=TIME_AVAILABILITY_CHOICES)
+    preferred_morning_time = models.TimeField(null=True, blank=True, help_text="Horas preferidas por la mañana")
+    preferred_evening_time = models.TimeField(null=True, black=True, help_text="Horas preferidas por la tarde")
+    motivation_level = models.IntegerField(
+        validators=[MinValueValidator(1),   MaxValueValidator(20)],
+        help_text="Nivel de motivacion de 1 ha 10"
+    )
+
+    user_objective = models.TextField(help_text="Objetivos principales del usuario")
+    experience_level_user = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(20)],
+        default=1,
+        help_text= "Nivel de experiencia del usuario"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_user_type_display()}"
+
+
+class Habits(models.Model):
+    FREQUENCY_CHOICES = [
+        ('Daily', 'Diario'),
+        ('Weekly', 'Semanal'),
+
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="habits")
+    title = models.CharField(max_length=260)
+    description = models.TextField(black=True)
+    frequency = models.CharField(max_length=20, choices=FREQUENCY_CHOICES)
+    target_minutes = models.PositiveIntegerField(
+        validators = [MaxValueValidator(1), MaxValueValidator(100)],
+        help_text = "Duracion en minutos"
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        user_index = [
+            models.Index(fields=['user', 'is_active', 'title', 'created_at'])
+        ]
+
+    def __str__(self):
+        return (f"{self.title} - "
+                f"{self.description} - "
+                f"{self.is_active}")
+
+
+class Habit_execution(models.Model):
+    STATUS_CHOICES = [
+        ('Completed', 'Completada'),
+        ('stand by', 'Pendiente'),
+        ('Skipped', 'Omitida'),
+        ('Not executed', 'Sin ejecutarse')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ModelUser_executions")
+    habit = models.ForeignKey(Habits, on_delete=models.CASCADE, related_name="habits_executions")
+    execution_date = models.DateField()
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default="Not executed")
+    duration_minutes = models.PositiveIntegerField(null=True, blank=True)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['habit', 'execution_date']
+        indexes = [
+            models.Index(fields=['user', 'execution_date']),
+            models.Index(fields=['habit', 'execution_date']),
+        ]
+
+    def __str__(self):
+        return f"{self.habit.title} - {self.execution_date}"
